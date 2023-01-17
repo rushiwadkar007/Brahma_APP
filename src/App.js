e1c97debc32d4b680f6b762afdbd29d143c32ed5
preeti13456 <preetijecrc99@outlook.com>
2023-01-17 21:12:46 +0530

    Publisher added

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import ReCAPTCHA from 'react-google-recaptcha';
+import React,{createContext, useContext, useRef} from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Layout } from './Layout';
+import { Login, loguser } from './Login';
+import { Signup } from './Signup';
+import { Forgpass } from './Forgpass';
+import { Wallet } from './wallet';
+import { render } from '@testing-library/react';
+import { Dashboard } from './Dashboard';
+
+export const luser=createContext();
+
+export default function App() {
+  const user=useContext(loguser);
+  return (
+    <luser.Provider value={user}>
+    <div className="App">
+      
+       
+       <BrowserRouter>
+         <Routes>
+             <Route path="/" element={<Layout/>}>
+              <Route index element={<Login flag="true"/>}/>
+              <Route path='/wallet' element={<Wallet/>}/>
+              <Route path='/Sign-up' element={<Signup flag="true"/>}/>
+              <Route path='/Forgot-pass' element={<Forgpass flag="true" />}/>
+              <Route path='/dashboard' element={<Dashboard/>}/>
+             </Route>
+         </Routes>
+       </BrowserRouter>
+    </div>
+    </luser.Provider>
+  );
+}
+
